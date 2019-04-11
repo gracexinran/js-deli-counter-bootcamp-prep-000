10b568d1f4e179e01a2b7054e2801bcab0d8bf80
Xinran Che <gracexinran@gmail.com>
2019-04-11 02:40:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function takeANumber(katzDeliLine,person){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) {
+      katzDeliLine.shift();
     return "Currently serving " + katzDeliLine[0] + ".";
   }
   else {
     return "There is nobody waiting to be served!"
   };
-  katzDeliLine.shift();
-  return katzDeliLine;
+
 }
